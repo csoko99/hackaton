ca90f04462c9c888f8e72dbbfe0eeecd3c267e78
csoko99 <laczkolali77@gmail.com>
2023-10-19 17:53:54 +0200

    Trying to remove the exe files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hackaton';
+  observable = new Observable<string>(observer => {
+    observer.next('Start Processing...');
+    setTimeout(() => observer.next('Still Processing...'), 3000);
+    setTimeout(() => observer.complete(), 5000);
+  });
+
+  ngOnInit() {
+    this.observable.subscribe({
+      next: data => console.log(data),
+      error: error => console.error(error),
+      complete: () => console.log('Process Completed!'),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import {ProductsService} from './products.service';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+
+
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
